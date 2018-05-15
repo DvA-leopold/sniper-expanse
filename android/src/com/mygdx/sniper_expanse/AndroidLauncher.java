90817e3df29010033f528879d8aa242fdc0daea4
Vlad Durnev <leopold.dva@gmail.com>
2018-05-15 22:09:17 +0300

    init commit

---
+++ android/src/com/mygdx/sniper_expanse/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.sniper_expanse;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.sniper_expanse.SniperExpanse;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SniperExpanse(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/sniper_expanse/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.sniper_expanse.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.sniper_expanse.SniperExpanse;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SniperExpanse(), config);
+	}
+}
